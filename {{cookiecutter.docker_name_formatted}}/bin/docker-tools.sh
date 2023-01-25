f30bcb5ca8609b0c861b35aaa768af7a71db8d81
Lee Myring <29373851+thinkstack@users.noreply.github.com>
2023-01-25 16:37:23 +0000

    TEL-1866: export version before running package
    Co-authored-by: Tim Fothergill <67912934+TimothyFothergill@users.noreply.github.com>

--- {{cookiecutter.docker_name_formatted}}/bin/docker-tools.sh
+++ {{cookiecutter.docker_name_formatted}}/bin/docker-tools.sh
@@ -53,6 +53,8 @@ cut_release() {
 package() {
   print_begins
 
+  export_version
+
   echo Authenticating with ECR
   aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
   echo Building the images
