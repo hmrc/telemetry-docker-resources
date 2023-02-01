67733f557b84e3ec656277ffcd95dd0c2cd0f41b
Lee Myring <29373851+thinkstack@users.noreply.github.com>
2023-02-01 11:19:59 +0000

    TEL-1866: massively simplify templating for build options
    Co-authored-by: Willem Veerman <6502426+willemveerman@users.noreply.github.com>

--- {{cookiecutter.docker_name_formatted}}/bin/docker-tools.sh
+++ {{cookiecutter.docker_name_formatted}}/bin/docker-tools.sh
@@ -9,109 +9,67 @@ set -o nounset
 #####################################################################
 ## Beginning of the configurations ##################################
 
-ACCOUNT_ID="{{ cookiecutter.internal_base_account_id }}"
-AWS_REGION="{{ cookiecutter.aws_region }}"
 BASE_LOCATION="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-PATH_BUILD="${BASE_LOCATION}/build"
-PROJECT_FULL_NAME="{{ cookiecutter.docker_name_formatted }}"
-REPO_NAME="{{ cookiecutter.docker_name_formatted }}"
+IMAGE_NAME="{{ cookiecutter.docker_image_name_formatted }}"
 
 ## End of the configurations ########################################
 #####################################################################
 
 debug_env(){
   echo BASE_LOCATION="${BASE_LOCATION}"
-  echo PROJECT_FULL_NAME="${PROJECT_FULL_NAME}"
-  echo PATH_BUILD="${PATH_BUILD}"
-}
-
-open_shell() {
-    print_begins
-
-    poetry export --without-hashes --format requirements.txt --with dev --output "requirements-tests.txt"
-    docker run -it \
-               --rm \
-               --volume "${BASE_LOCATION}":/data \
-               --workdir /data \
-               --env REQUIREMENTS_FILE="requirements-tests.txt" \
-               --env VENV_NAME="venv" \
-               python:$(cat "${BASE_LOCATION}/.python-version")-slim-buster /data/bin/entrypoint.sh /bin/bash
-
-    print_completed
+  echo IMAGE_NAME="${IMAGE_NAME}"
 }
 
 # Creates a release tag in the repository
 cut_release() {
   print_begins
-
   poetry run cut-release
-
   print_completed
 }
 
 # Build the Docker image
 package() {
   print_begins
-
   export_version
 
   echo Building the images
 
-{% if cookiecutter.docker_build_info_default is defined and cookiecutter.docker_build_info_default|length %}
-  {%- for key, value in cookiecutter.docker_build_info_default|dictsort %}
-  docker build --tag "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${VERSION}"
-    {%- if value.build_args is defined and value.build_args|length %}
-      {%- for build_arg in value.build_args%} --build-arg {{build_arg|safe}} {%- endfor %}
-    {%- endif %}
-    {%- if value.platform is defined and value.platform|length %}
-      {%- for platform in value.platform%} --platform {{platform|safe}} {%- endfor %}
-    {%- endif %} .
-  {%- endfor %}
-{% else %}
-  docker build --tag "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${VERSION}" .
-{% endif %}
-
-{% if cookiecutter.docker_build_info_additional is defined and cookiecutter.docker_build_info_additional|length %}
-  {%- for key, value in cookiecutter.docker_build_info_additional|dictsort %}
-  docker build --tag "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${VERSION}{{key|safe}}"
-    {%- if value.build_args is defined and value.build_args|length %}
-      {%- for build_arg in value.build_args%} --build-arg {{build_arg|safe}} {%- endfor %}
-    {%- endif %}
-    {%- if value.platform is defined and value.platform|length %}
-      {%- for platform in value.platform%} --platform {{platform|safe}} {%- endfor %}
-    {%- endif %} .
-  {%- endfor %}
-{% endif %}
-
+{%- if cookiecutter.docker_include_default_build is sameas true %}
+  {%- if cookiecutter.docker_build_options_default is defined and cookiecutter.docker_build_options_default|length %}
+  docker build --tag "{{cookiecutter.aws_account_id}}.dkr.ecr.{{cookiecutter.aws_region}}.amazonaws.com/{{cookiecutter.docker_image_name_formatted}}:${VERSION}" {{cookiecutter.docker_build_options_default|safe}} .
+  {%- else %}
+  docker build --tag "{{cookiecutter.aws_account_id}}.dkr.ecr.{{cookiecutter.aws_region}}.amazonaws.com/{{cookiecutter.docker_image_name_formatted}}:${VERSION}" .
+  {%- endif %}
+{%- endif %}
+{%- for key, value in cookiecutter.docker_build_options_additional|dictsort %}
+  docker build --tag "{{cookiecutter.aws_account_id}}.dkr.ecr.{{cookiecutter.aws_region}}.amazonaws.com/{{cookiecutter.docker_image_name_formatted}}:${VERSION}{{key|safe}}" {{value|safe}} .
+{%- endfor %}
   print_completed
 }
 
 # Bump the function's version when appropriate
 prepare_release() {
   print_begins
-
   poetry run prepare-release
   export_version
-
   print_completed
 }
 
 publish_to_ecr() {
   print_begins
-
   export_version
 
   echo Authenticating with ECR
-  aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+  aws ecr get-login-password --region "{{cookiecutter.aws_region}}" | docker login --username AWS --password-stdin "{{cookiecutter.aws_account_id}}.dkr.ecr.{{cookiecutter.aws_region}}.amazonaws.com"
+
   echo Pushing the images
 
-  docker push "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${VERSION}"
-{% if cookiecutter.docker_build_info_additional is defined and cookiecutter.docker_build_info_additional|length %}
-  {%- for key, value in cookiecutter.docker_build_info_additional|dictsort %}
-  docker push "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${VERSION}{{key|safe}}"
-  {%- endfor %}
-{% endif %}
-
+{%- if cookiecutter.docker_include_default_build is sameas true %}
+  docker push "{{cookiecutter.aws_account_id}}.dkr.ecr.{{cookiecutter.aws_region}}.amazonaws.com/{{cookiecutter.docker_image_name_formatted}}:${VERSION}"
+{%- endif %}
+{%- for key, value in cookiecutter.docker_build_options_additional|dictsort %}
+  docker push "{{cookiecutter.aws_account_id}}.dkr.ecr.{{cookiecutter.aws_region}}.amazonaws.com/{{cookiecutter.docker_image_name_formatted}}:${VERSION}{{key|safe}}"
+{%- endfor %}
   print_completed
 }
 
@@ -152,8 +110,7 @@ print_completed() {
 
 print_configs() {
   echo -e "BASE_LOCATION:\t\t\t${BASE_LOCATION}"
-  echo -e "PATH_BUILD:\t\t\t${PATH_BUILD}"
-  echo -e "PROJECT_FULL_NAME:\t\t${PROJECT_FULL_NAME}"
+  echo -e "IMAGE_NAME:\t\t${IMAGE_NAME}"
 }
 
 ## End of the helper methods ########################################
@@ -165,11 +122,8 @@ main() {
   # Validate command arguments
   [ "$#" -ne 1 ] && help && exit 1
   function="$1"
-  functions="help cut_release debug_env open_shell package prepare_release print_configs publish_to_ecr"
+  functions="help cut_release debug_env package prepare_release print_configs publish_to_ecr"
   [[ $functions =~ (^|[[:space:]])"$function"($|[[:space:]]) ]] || (echo -e "\n\"$function\" is not a valid command. Try \"$0 help\" for more details" && exit 2)
-
-  # Ensure build folder is available
-  mkdir -p "${PATH_BUILD}"
 
   $function
 }
